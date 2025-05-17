900134201717b49b488e613693ab4b33add83270
delprafede <delpradofederico0@gmail.com>
2025-05-16 22:41:14 -0300

    guardando cambios para agregar productos en titulo

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -21,9 +21,9 @@ const Products = () => {
 
   return (
     <>
-    {products.length === 0 && <h2 className="text-4xl font-extrabold text-slate-500">No hay productos</h2>}
+   
       <div className="flex justify-between">
-        <h2 className=" text-4xl font-extrabold text-slate-500 ">Productos</h2>
+        {products.length === 0 ? <h2 className="text-4xl font-extrabold text-slate-500">No hay productos</h2> : <h2 className="text-4xl font-extrabold text-slate-500">Productos</h2>}
         <Link
           to="/products/new"
           className="bg-blue-600 text-white p-3 rounded-md shadow-sm font-bold uppercase text-sm hover:bg-blue-700 transition-colors"
